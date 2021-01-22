97e218aa692a7f7adc09ef65ff18306c8a4b043d
SergioAlmazan <sergioalmazan18@yahoo.com>
2021-01-22 15:36:16 -0600

    Added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('double-double', false);
